dfc4571ef02b851dc1b282964039c69d7bed4633
hmb <mstfhll353@gmail.com>
2023-07-02 01:53:17 +0300

    Add data view

--- TravelBook/TravelBook/TravelBook/ListViewController.swift
+++ TravelBook/TravelBook/TravelBook/ListViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    var chosenTitle = ""
+    var chosenTitleId : UUID?
     
     
     override func viewDidLoad() {
@@ -21,24 +25,63 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
 
+        getData()
+    }
+    
+    @objc func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    if let title = result.value(forKey: "title") as? String{
+                        self.titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        catch{
+            print("Error")
+        }
     }
     
     @objc func addButtonClicked(){
-        
+        chosenTitle = ""
         performSegue(withIdentifier: "toViewController", sender: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenTitle = titleArray[indexPath.row]
+        chosenTitleId = idArray[indexPath.row]
+            performSegue(withIdentifier: "toViewController", sender: nil)
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toViewController"{
+            let destionationVc = segue.destination as! ViewController
+            destionationVc.selectedTitle = chosenTitle
+            destionationVc.selectedTitleID = chosenTitleId
+        }
+    }
 
 }
--- TravelBook/TravelBook/TravelBook/ViewController.swift
+++ TravelBook/TravelBook/TravelBook/ViewController.swift
@@ -21,7 +21,16 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     var chosenLatitude = Double()
     var chosenLongitude = Double()
+    
     var locationManager = CLLocationManager()
+    
+    var selectedTitle = ""
+    var selectedTitleID: UUID?
+    
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +48,60 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
         gestureRecognizer.minimumPressDuration = 3
         mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if selectedTitle != ""{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            let idString = selectedTitleID!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = @", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let title = result.value(forKey: "title") as? String{
+                            annotationTitle = title
+                            if let subtitle = result.value(forKey: "subtitle") as? String{
+                                annotationSubtitle = subtitle
+                                if let latitude = result.value(forKey: "latitude") as? Double{
+                                    annotationLatitude = latitude
+                                    if let longitude = result.value(forKey: "longitude") as? Double{
+                                        annotationLongitude = longitude
+                                        
+                                        let annotation = MKPointAnnotation()
+                                        annotation.title = annotationTitle
+                                        annotation.subtitle = annotationSubtitle
+                                        let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                                        annotation.coordinate = coordinate
+                                        
+                                        mapView.addAnnotation(annotation)
+                                        nameText.text = annotationTitle
+                                        commentText.text = annotationSubtitle
+                                        
+                                        locationManager.stopUpdatingLocation()
+                                        
+                                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                                        mapView.setRegion(region, animated: true)
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            catch {
+                print("error")
+            }
+        }
+        else{
+            
+        }
+        
     }
     
     @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer){
