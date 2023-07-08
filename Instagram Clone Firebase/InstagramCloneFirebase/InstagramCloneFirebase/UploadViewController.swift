d7105b739db13397bc369184daa4e75de3874f49
hmb <mstfhll353@gmail.com>
2023-07-08 22:34:55 +0300

    Upload image added

--- Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/UploadViewController.swift
+++ Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/UploadViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
+    
+    @IBOutlet weak var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Kullanıcının resmi tıklanabilir hale getirmesini sağlıyoruz öncelikle.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage) )
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Resim seçici fonksiyon
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
-    */
+    
+    //Resmi seçtikten sonra ne olacağını belirleyen fonksiyon.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func actionButtonClicked(_ sender: Any) {
+    }
+    
+   
 
 }
