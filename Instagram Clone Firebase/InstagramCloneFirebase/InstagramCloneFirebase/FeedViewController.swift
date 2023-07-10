73b0632721492895a60b161e1a5d7a87a2e8fb89
hmb <mstfhll353@gmail.com>
2023-07-10 15:41:15 +0300

    Instagram Post Upload added

--- Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/FeedViewController.swift
+++ Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/FeedViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+   
+  
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = "user@mail.com"
+        cell.likeLabel.text = "0"
+        cell.commentLabel.text = "Comment"
+        cell.userImageView.image = UIImage(named: "tapToSelectIcon")
+        return cell
+    }
 
+    
 }
--- Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/UploadViewController.swift
+++ Instagram Clone Firebase/InstagramCloneFirebase/InstagramCloneFirebase/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -40,7 +42,58 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.dismiss(animated: true, completion: nil)
     }
     
+    //Hata bildirim fonksiyonu.
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    //Upload butonuna tıklandığında çağırılan fonksiyon. Firebase Storage'a resmi yükler  ve resim yüklendikten sonra resmin indirme bağlantısını verir.
     @IBAction func actionButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    imageReference.downloadURL{(url, error) in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            //Verileri oluşturulan veritabanına kaydetme.
+                            
+                            let fireStoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy": Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
+                            
+                            firestoreReference = fireStoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: {
+                                (error) in
+                                if error != nil{
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                }else{
+                                    self.imageView.image = UIImage(named: "tapToSelectIcon")
+                                    self.commentText.text = ""
+                                    self.tabBarController?.selectedIndex = 0
+                                }
+                            })
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
     
    
