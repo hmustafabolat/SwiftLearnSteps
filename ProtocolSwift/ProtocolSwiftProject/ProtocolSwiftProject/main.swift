fe7fff015bcb3e5e39dfe8541dbbf6453c8a71e3
hmb <mstfhll353@gmail.com>
2023-07-22 02:31:38 +0300

    Closures Added

--- ProtocolSwift/ProtocolSwiftProject/ProtocolSwiftProject/main.swift
+++ ProtocolSwift/ProtocolSwiftProject/ProtocolSwiftProject/main.swift
@@ -5,7 +5,50 @@
 //  Created by Musti on 22.07.2023.
 //
 
+
 import Foundation
 
-print("Hello, World!")
+protocol Running {
+    func myRun()
+}
 
+class Animal {
+    func test() {
+        print("test")
+    }
+}
+
+class Dog : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let barley = Dog()
+barley.myRun()
+
+class Cat : Animal, Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let cat = Cat()
+cat.test()
+cat.myRun()
+
+class Turtle : Animal {
+    
+}
+
+let leonardo = Turtle()
+
+
+struct Bird : Running {
+    func myRun() {
+        print("running")
+    }
+}
+
+let tweety = Bird()
+tweety.myRun()
