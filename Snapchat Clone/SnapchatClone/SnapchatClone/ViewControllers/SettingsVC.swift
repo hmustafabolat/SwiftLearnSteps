78677bff7f7c5b6a7ad4e5742e4b204cc3926d60
hmb <mstfhll353@gmail.com>
2023-07-24 17:07:01 +0300

    SnapChat Clone App Added

---
+++ Snapchat Clone/SnapchatClone/SnapchatClone/ViewControllers/SettingsVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsVC.swift
+//  SnapchatClone
+//
+//  Created by Musti on 24.07.2023.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+    }
+    
+    
+   
+}
---
+++ Snapchat Clone/SnapchatClone/SnapchatClone/ViewControllers/UploadVC.swift
@@ -0,0 +1,26 @@
+//
+//  UploadVC.swift
+//  SnapchatClone
+//
+//  Created by Musti on 24.07.2023.
+//
+
+import UIKit
+
+class UploadVC: UIViewController {
+    
+    
+    @IBOutlet weak var uploadImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func uploadClicked(_ sender: Any) {
+    }
+    
+    
+
+}
