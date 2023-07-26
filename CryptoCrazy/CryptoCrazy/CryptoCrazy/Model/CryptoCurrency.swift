91ae22fd28fe3ca975b15aea344a00402d13dcbb
hmb <mstfhll353@gmail.com>
2023-07-27 01:37:28 +0300

    Crypto App Added

---
+++ CryptoCrazy/CryptoCrazy/CryptoCrazy/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazy
+//
+//  Created by Musti on 27.07.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency : Decodable {
+    let currency : String
+    let price : String
+}
---
+++ CryptoCrazy/CryptoCrazy/CryptoCrazy/Service/Webservices.swift
@@ -0,0 +1,16 @@
+//
+//  Webservices.swift
+//  CryptoCrazy
+//
+//  Created by Musti on 27.07.2023.
+//
+
+import Foundation
+
+
+class Webservice{
+    
+    func downloadCurrenciew(){
+        
+    }
+}
