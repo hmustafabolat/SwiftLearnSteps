a1207138a40316a421053568f807987fb7bda211
hmb <mstfhll353@gmail.com>
2023-07-27 15:01:23 +0300

    Web Services and Data read added

--- CryptoCrazy/CryptoCrazy/CryptoCrazy/Service/Webservices.swift
+++ CryptoCrazy/CryptoCrazy/CryptoCrazy/Service/Webservices.swift
@@ -10,7 +10,19 @@ import Foundation
 
 class Webservice{
     
-    func downloadCurrenciew(){
-        
+    func downloadCurrencies(url: URL, completion: @escaping([CryptoCurrency]?) -> ()){
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }else if let data = data {
+                //Bütün veri işlemleri burdan dönüyor
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                print(cryptoList)
+                if let cryptoList = cryptoList{ completion(cryptoList)
+                }
+            }
+            //resume işlemi başlatmamıza olanak tanıyan yapı.
+        }.resume()
     }
 }
