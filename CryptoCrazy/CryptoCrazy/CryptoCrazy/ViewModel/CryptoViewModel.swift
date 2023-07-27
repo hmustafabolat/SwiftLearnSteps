a144654b7abae1e063fa758ee371549c99cbc69b
hmb <mstfhll353@gmail.com>
2023-07-27 17:59:26 +0300

    Finished Crypto App

---
+++ CryptoCrazy/CryptoCrazy/CryptoCrazy/ViewModel/CryptoViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  CryptoViewModel.swift
+//  CryptoCrazy
+//
+//  Created by Musti on 27.07.2023.
+//
+
+import Foundation
+
+struct CryptoListViewModel{
+    let cryptoCurrencyList : [CryptoCurrency]
+    
+    func numberOfRowInSection () -> Int {
+        return self.cryptoCurrencyList.count
+    }
+    
+    func cryptoAtIndex(_ index : Int) -> CryptoViewModel{
+        let crypto = self.cryptoCurrencyList[index]
+        return CryptoViewModel(cryptoCurrency: crypto)
+    }
+}
+
+struct CryptoViewModel{
+    let cryptoCurrency : CryptoCurrency
+    
+    var name : String {
+        return self.cryptoCurrency.currency
+    }
+    
+    var price : String {
+        return self.cryptoCurrency.price
+    }
+}
